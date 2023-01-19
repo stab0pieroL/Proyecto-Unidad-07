36cf32cb8c80c900db1e558c7bfe2cd086b2c056
stab0pieroL <rake.pumpkinhead.96@gmail.com>
2023-01-18 20:26:38 -0500

    inicio del proyecto de playlist

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor se ejecuta en http://localhost:${port}`);
+});
