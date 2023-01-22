38d3607bc415210e175bb71f11f2cadc5c572186
stab0pieroL <rake.pumpkinhead.96@gmail.com>
2023-01-21 22:46:27 -0500

    modificando schema y post login

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config();
 
 const app: Express = express();
+const prisma = new PrismaClient()
 const port = process.env.PORT;
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/api/v1/users/login',async (req, res) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+    },
+  })
+  res.json(user);
 });
 
 app.listen(port, () => {
   console.log(`El servidor se ejecuta en http://localhost:${port}`);
-});
+})
