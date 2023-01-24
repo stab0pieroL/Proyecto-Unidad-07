bf673e0444ead9bab108877c009a54785d3f86b9
stab0pieroL <rake.pumpkinhead.96@gmail.com>
2023-01-24 12:46:17 -0500

    rutas post: playlist, playlist/:id  y songs, y las rutas get:songs y songs/:id

--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt';
-
-
-
+const SECRET_KEY = "mysecretkey";
 
 dotenv.config();
 const app: Express = express();
@@ -17,10 +15,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-  res.send('');
-});
-
+//creacion de usuario
 
 app.post('/api/v1/users/', async (req, res) => {
 
@@ -36,6 +31,7 @@ app.post('/api/v1/users/', async (req, res) => {
   res.json(user);
 });
 
+//Login de usuarios
 
 app.post('api/v1/users/login', async (req, res) => {
 
@@ -51,6 +47,115 @@ app.post('api/v1/users/login', async (req, res) => {
   }
 });
 
+//Creación de playlists
+
+app.post("/api/v1/playlists", async (req, res) => {
+  const { title, user_id } = req.body;
+  const playlist = await prisma.playlist.create({
+    data: {
+      title,
+      user: {
+        connect: {
+          id: user_id,
+        },
+      },
+    },
+  });
+  res.json(playlist);
+});
+
+//Agregar canción a la playlist deseada
+
+app.post("api/v1/playlists/:id/songs", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {songId} = req.body;
+    const playlist = await prisma.playlist.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!playlist) {
+      return res.status(404).json({ error: "Playlist not found" });
+    }
+
+    const song = await prisma.song.findOne({
+      where: {
+        id:songId
+      },
+    });
+
+    if (!song) {
+      return res.status(404).json({ error: "Song not found" });
+    }
+
+    const playlistSong = await prisma.playlistSong.create({
+      data: {
+        song: {
+          connect: {
+            id:songId
+          }
+        },
+        playlist: {
+          connect: {
+            id
+          }
+        },
+      },
+    });
+
+    return res.json(playlistSong);
+  } catch (error) {
+    return res.status(500).json({ error: "Error" });
+  }
+});
+
+//Crear canciones
+
+app.post("/api/v1/songs", async (req, res) => {
+  try {
+    const { name, artist, album, year, genre, duration, privacy } = req.body;
+
+    const song = await prisma.song.create({
+      data: {
+        name,
+        artist,
+        album,
+        year,
+        genre,
+        duration,
+        privacy
+      },
+    });
+
+    return res.json(song);
+  } catch (error) {
+    return res.status(500).json({ error: "Error" });
+  }
+});
+
+//Obtener lista de  las canciones agregadas
+
+app.get("/songs", async (req, res) => {
+  const songs = await prisma.song.findMany();
+  res.json(songs);
+});
+
+//Obtener canción por id
+
+app.get("/songs/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await prisma.song.findOne({
+    where: {
+      id,
+    },
+  });
+  if (!song) {
+    return res.status(404).json({ error: "Song not found" });
+  }
+  res.json(song);
+});
 
 app.listen(port, () => {
   console.log(`El servidor se ejecuta en http://localhost:${port}`);
